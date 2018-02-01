8dcf600827b79b799e6b49da74d1c5c6fc5e4586
eriksmith <erik-smith@outlook.com>
2018-02-01 12:15:27 +0100

    .

--- page/login.php
+++ page/login.php
@@ -33,7 +33,7 @@ if (!empty($_POST)){
 		while($row = mysqli_fetch_assoc($result)) {
 		echo "Je bent nu ingelogd als: ". $row['Naam'];
 		$_SESSION["naam"] = $row['naam'];
-		$_SESSION["admin"] = $row['admin'];
+		$_SESSION["functie"] = $row['functies_idfunctie'];
 		
 		header('Location:?page=home');
 	}
